bf1be64b81bcf79b8f63863339e9c3f886daa620
Alexandru Andronache <alexandru@utilmond.dev>
2022-07-20 20:58:03 +0300

    Refactor exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import createRouterMiddleware from './middleware';
-import createRouterSlice from './slice';
-import * as selectors from './selectors';
-
-export default {
-  createRouterMiddleware,
-  createRouterSlice,
-  selectors,
-};
+export * from './middleware';
+export * from './slice';
+export * from './selectors';
