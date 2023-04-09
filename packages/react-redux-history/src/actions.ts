f9aa5a1aa04fd79b278b65324bc0f6d8b5af6ebf
Isis☕Caffe <52106389+fum4@users.noreply.github.com>
2023-04-09 13:50:52 +0300

    fix declaration files

--- packages/react-redux-history/src/actions.ts
+++ packages/react-redux-history/src/actions.ts
@@ -1,21 +1,23 @@
-import { Action, type To } from 'history';
+import { type To, Action } from 'history';
+import type { Dispatch } from 'redux';
+
 import { LOCATION_CHANGE_REQUEST } from './types';
 
-export const push = (to: To, state?: any) => (dispatch) => {
+export const push = (to: To, state?: any) => (dispatch: Dispatch) => {
   const location = typeof to === 'string' ? { pathname: to, state } : { ...to, state };
   const payload = { location, type: Action.Push };
 
   dispatch({ type: LOCATION_CHANGE_REQUEST, payload });
 };
 
-export const replace = (to: To, state?: any) => (dispatch) => {
+export const replace = (to: To, state?: any) => (dispatch: Dispatch) => {
   const location = typeof to === 'string' ? { pathname: to, state } : { ...to, state };
   const payload = { location, type: Action.Replace };
 
   dispatch({ type: LOCATION_CHANGE_REQUEST, payload });
 };
 
-export const go = (delta: number) => (dispatch) => {
+export const go = (delta: number) => (dispatch: Dispatch) => {
   const payload = { delta, type: Action.Pop };
 
   dispatch({ type: LOCATION_CHANGE_REQUEST, payload });
