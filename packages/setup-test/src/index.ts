d1e40d937d28990a773256aed9b11082bada2453
fum4 <andronache.alex22@gmail.com>
2023-05-20 12:29:29 +0300

    [setup-test] - alpha implementation

--- packages/setup-test/src/index.ts
+++ packages/setup-test/src/index.ts
@@ -1,5 +1,63 @@
-const setupTest = () => {
-  console.log("Test environment configured!");
+import merge from 'lodash/merge';
+
+interface Configuration {
+  render: () => any;
+  renderOptions?: any;
+  deepMerge?: boolean;
+  handlers: Handlers;
+}
+
+interface Handlers {
+  [string]: HandlerConfig;
+}
+
+interface HandlerConfig {
+  handler: (...args: any) => any;
+  defaultValue?: any;
+}
+
+const setupTestEnvironment = <T = Configuration>(defaultConfig: T) => {
+  return (component: any, config: any) => {
+    return createSetup(component, config, defaultConfig);
+  }
 };
 
-export default setupTest;
+const createSetup = <T>(
+  component: any,
+  config: { [string]: any, deepMerge?: boolean },
+  defaultConfig: HandlerConfig
+) => {
+  return ({
+    ...config,
+    deepMerge = defaultConfig.deepMerge = true,
+    renderOptions = defaultConfig.renderOptions
+  }) => {
+    const handlers = Object.entries(defaultConfig.handlers).map(([ name, defaultHandlerConfig ]) => ({
+      handler: defaultHandlerConfig.handler,
+      value: deepMerge
+        ? merge({}, defaultHandlerConfig.defaultValue, config[name])
+        : config[name] || defaultHandlerConfig.defaultValue
+    }));
+
+    return setupTest({
+      component,
+      handlers,
+      render,
+      renderOptions: deepMerge
+        ? merge({}, defaultConfig.renderOptions, renderOptions)
+        : renderOptions || defaultConfig.renderOptions
+    });
+  }
+};
+
+const setupTest = ({ component, handlers, render, renderOptions }) => {
+  handlers.forEach(({ handler, value }) => {
+    if (value) {
+      handler(value);
+    }
+  });
+
+  return render(component, ...renderOptions);
+}
+
+export default setupTestEnvironment;
